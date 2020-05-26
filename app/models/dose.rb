bf9f6cceefe4c7cb08c30f7474edad7eae8e1f70
bart boillot <barthboillot@hotmail.com>
2020-05-26 21:28:30 +0200

    change validation

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,6 +3,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   validates :cocktail_id, presence: true
   validates :ingredient_id, presence: true
-  validates :description, presence: true
+  validates :measurement, presence: true
   validates :cocktail_id, uniqueness: { scope: :ingredient_id }
 end
